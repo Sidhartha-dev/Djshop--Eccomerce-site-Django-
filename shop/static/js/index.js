a77dde1c5fa97408eeae9a654f2c14b3d4cb6bd8
kordomon07@gmail.com <kordomon07@gmail.com>
2020-04-20 20:36:41 +0530

    Update add remove and not going minous value solve

--- shop/static/js/index.js
+++ shop/static/js/index.js
@@ -1,22 +1,60 @@
 //console.log("working");
+//find the cart from lstorage
 if (localStorage.getItem("cart") == null) {
   var cart = {};
 } else {
   cart = JSON.parse(localStorage.getItem("cart"));
   document.getElementById("cart").innerHTML = Object.keys(cart).length;
+  updateCart(cart);
 }
 
-$(".cart").click(function(){
+//if click in add to cart then increment the item
+$(".cart").click(function () {
   //console.log("clicked");
   var idstr = this.id.toString();
-  console.log(idstr);
+  //console.log(idstr);
   if (cart[idstr] != undefined) {
     cart[idstr] = cart[idstr] + 1;
   } else {
     cart[idstr] = 1;
   }
-  console.log(cart);
+  updateCart(cart);
+  //console.log(cart);
   localStorage.setItem("cart", JSON.stringify(cart));
   document.getElementById("cart").innerHTML = Object.keys(cart).length;
 });
 
+function updateCart(cart) {
+  for (var item in cart) {
+    document.getElementById("div" + item).innerHTML =
+      "<button id='minus" +
+      item +
+      "' class='btn btn-warning rounded-pill shadow minus'>-</button> <span id='val" +
+      item +
+      "''>" +
+      cart[item] +
+      "</span> <button id='plus" +
+      item +
+      "' class='btn btn-primary rounded-pill shadow plus'> + </button>";
+  }
+}
+
+//= plas minas button working trick
+$(".divpr").on("click", "button.minus", function () {
+  //console.log("minus mio");
+  a = this.id.slice(7);
+  //console.log(a);
+  cart["pr" + a] = cart["pr" + a] - 1;
+  cart["pr" + a] = Math.max(0, cart["pr" + a]);
+  document.getElementById("valpr" + a).innerHTML = cart["pr" + a];
+  updateCart(cart);
+});
+
+$(".divpr").on("click", "button.plus", function () {
+  //console.log("plus clicked");
+  a = this.id.slice(6);
+  //console.log(a);
+  cart["pr" + a] = cart["pr" + a] + 1;
+  document.getElementById("valpr" + a).innerHTML = cart["pr" + a];
+  updateCart(cart);
+});
