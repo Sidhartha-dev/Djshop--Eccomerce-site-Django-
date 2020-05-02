2dbab5838976d2753b17520a34fa029ebd5e2562
kordomon07@gmail.com <kordomon07@gmail.com>
2020-05-02 18:41:22 +0530

    fetching blogpost from sqlite

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='blogHome'),
-    path('blogPost/', views.blogpost, name='blogPost'),
+    path('blogpost/<int:id>', views.blogpost, name='blogPost'),
 ]
